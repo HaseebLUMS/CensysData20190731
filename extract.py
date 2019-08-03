efec4ba34b55569ec12729ceda1910ad1f176be0
Muhammad Haseeb <haseeb.lums@gmail.com>
2019-08-03 19:29:57 +0500

    ready for server

---
+++ extract.py
@@ -0,0 +1,41 @@
+import json
+import random
+
+def giveFileName():
+	token = 'censysData_0000000000'
+	# r = random.randint(0, 100)
+	for r in range(0, 99):
+		r = str(r)
+		if len(r) == 1:
+			yield token + '0' + r + '.json'
+		if len(r) == 2:
+			yield token + r + '.json'
+
+def giveRecord():
+	for file in giveFileName():
+		print('\n\n\n\n\n\n')
+		with open(file) as f: data = f.read()
+		data = data.split("\n")
+		for d in data:
+			try:
+				d = json.loads(d)
+				yield d
+			except:
+				pass
+
+def getIPs():
+	ips = []
+	count = 0
+	COUNT = 0
+	ipDictionary = {}
+
+	for ele in giveRecord():
+		try:
+			if ('ftpBanner' in ele) or ('telnetBanner' in ele):
+				if 'description' in ele:
+					ipDictionary[ele['ip']] = ele
+		except:
+			pass
+	ipDictionary = json.dumps(ipDictionary, indent=4)
+	with open('ipDictionary.json', 'w') as f: f.write(ipDictionary)
+getIPs()
