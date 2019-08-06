26c7173e61a58a2e3c8ad39f9451af402d242878
HaseebLUMS <haseeb.lums@gmail.com>
2019-08-06 18:59:57 +0500

    processing all on a server

--- extract.py
+++ extract.py
@@ -4,7 +4,7 @@ import random
 def giveFileName():
 	token = 'censysData_0000000000'
 	# r = random.randint(0, 100)
-	for r in range(0, 2):
+	for r in range(0, 100):
 		r = str(r)
 		if len(r) == 1:
 			yield token + '0' + r + '.json'
