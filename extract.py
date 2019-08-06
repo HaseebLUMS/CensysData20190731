ff19b07637b1ae52aa6d20108501ca10fc32376d
HaseebLUMS <haseeb.lums@gmail.com>
2019-08-06 19:04:11 +0500

    processing all on a server

--- extract.py
+++ extract.py
@@ -4,7 +4,7 @@ import random
 def giveFileName():
 	token = 'censysData_0000000000'
 	# r = random.randint(0, 100)
-	for r in range(0, 100):
+	for r in range(0, 20):
 		r = str(r)
 		if len(r) == 1:
 			yield token + '0' + r + '.json'
@@ -12,8 +12,10 @@ def giveFileName():
 			yield token + r + '.json'
 
 def giveRecord():
+	i = 1
 	for file in giveFileName():
-		print('\n\n\n\n\n\n')
+		print('file: ', i)
+		i+=1
 		with open(file) as f: data = f.read()
 		data = data.split("\n")
 		for d in data:
@@ -34,7 +36,6 @@ def getIPs():
 			if ('ftpBanner' in ele) or ('telnetBanner' in ele):
 				if 'description' in ele:
 					ipDictionary[ele['ip']] = ele
-
 		except:
 			pass
 	ipDictionary = json.dumps(ipDictionary, indent=4)
